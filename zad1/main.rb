6759544c19d1509b95d6be8107eb4cfe44351687
Kasia <szubskimateusz@gmail.com>
2018-11-02 16:47:06 +0100

    Rozwiązanie zadania 1

--- zad1/main.rb
+++ zad1/main.rb
@@ -1,21 +1,20 @@
-def sort_letters(string)
-
+#Zadanie 1
+def sort_letters
+sortuj = [ 'h', 'a', 'c', 'z', 'v' ]
+puts sortuj.sort.join(' ')
 end
-
-def count_vowels(string)
-
+puts "Zadanie 1\nPosortowany ciąg znaków:\n"
+sort_letters
+#Zadanie 2
+def count_vowels(str)
+  str.scan(/[aeouiy]/).count
 end
-
-def filter_odd(array)
-
+puts "Zadanie 2\nLiczba samogłosek w słowie ananasowy:\n"
+puts count_vowels("ananasowy")
+#Zadanie 3
+def odd_numbers
+array = [1,2,3,4,5]
+p array.sort.reject {|liczba| liczba.even?}
 end
-
-sort_letters('dcba') #=> 'abcd'
-sort_letters('zyx') #=> 'xyz'
-
-count_vowels('mmmm') #=> 0
-count_vowels('super') #=> 2
-count_vowels('super extra') #=> 4
-
-filter_odd([1, 2, 3, 4, 5]) #=> [1, 3, 5]
-filter_odd([6, 7, 9, 2, 6, 5]) #=> [7, 9, 5]
+puts "Zadanie 3\nTablica liczb nieparzystych:\n"
+odd_numbers
